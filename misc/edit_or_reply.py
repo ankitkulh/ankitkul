0ab5b7a5ffad72215b1f41f927db220567f5d403
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-05-11 12:43:37 +0530

    Delete edit_or_reply.py

--- misc/edit_or_reply.py
+++
@@ -1,9 +0,0 @@
-#Let it decide edit or reply for sudo filters
-#😂🤣
-from sample_config import Config
-
-def edit_or_reply(client, user_id, text):
-    if user_id in Config.SUDO_USERS:
-        return client.reply(text)
-    else:
-        return client.edit(text)
