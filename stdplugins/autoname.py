fbd8d003e18c879182dcd9fdb8f30ae2dcd3ca10
ankitkulh <64985777+ankitkulh@users.noreply.github.com>
2020-06-07 04:37:20 +0530

    Update autoname.py

--- stdplugins/autoname.py
+++ stdplugins/autoname.py
@@ -5,7 +5,8 @@ import asyncio
 import time
 from telethon.tl import functions
 from telethon.errors import FloodWaitError
-from userbot.utils import admin_cmd
+from uniborg.util import admin_cmd
+
 
 
 DEL_TIME_OUT = 60
